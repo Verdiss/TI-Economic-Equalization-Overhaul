6c882596dc4e1776599f2834d38000cca2ff037f
gorion.b <gorion.b@gmail.com>
2022-10-19 13:34:14 -0500

    Reworked GDP growth system, is now exponential decay off of gdp per capita, and resource/economic regions give diminishing returns

--- TIEconomyMod/EconomyInvestmentPatches/EconomyGDPEffectPatch.cs
+++ TIEconomyMod/EconomyInvestmentPatches/EconomyGDPEffectPatch.cs
@@ -15,40 +15,35 @@ namespace TIEconomyMod
         [HarmonyPrefix]
         public static bool GetEconomyPriorityPerCapitaIncomeChangeOverwrite(ref float __result, TINationState __instance)
         {
-            //This patch changes the economy investment's GDP effect from primarily a per capita GDP effect to a primarily national GDP effect, which is then distributed per capita
-            //The end goal is for a country to achieve roughly 1-2% GDP growth every month, after modifiers, when at 100% econony investment priority
-            
-            //A economy investment costs 1 IP, and an IP captures 10 Bn GDP-months, so it represents 10 Bn GDP-months
-            //Thus if about 0.1-0.2 Bn GDP is gained every economy investment completion, the growth of GDP will be the desired 1-2% monthly at 100% economy priority
-            //Orgs, Habs, and Tech will increase this amount, so a growth rate of 3-4% monthly is likley attainable at endgame, allowing for massive economic shifts.
-            //Keep in mind that most of the time 100% economy investment is not viable, so actual growth rates will be much smaller barring an extreme push
-            
-            //Example: A country has 1,000 Bn GDP and 20 Mn population. It thus has 100 IP. It can complete 100 economy investments a month.
-            //Each investment gives 0.2 Bn GDP after all calculations, so over the course of the month it gains 100 * 0.2 Bn = 20 Bn GDP, 2% of its starting GDP.
-            //Its population is 20 million, so the per capita GDP increase is $1000 over the course of the month.
+            //This patch changes the economy investment's GDP effect from increasing gdp per capita by a flat(ish) amount, to increasing gdp by a flat(ish) amount and distributing that across the pop as gdp per capita
+            //The most significant change is that GDP growth is dependent on an exponential decay function off of per capita GDP. This makes developing poor countries much more effective than developing rich ones, accounting for all factors
 
-            //Example 2: The country is one one-hundredth the size, with equal other stats. It has 10 Bn GDP and 1 IP a month.
-            //It can complete 1 economy investment a month at 100% priority. That gives 0.2 Bn GDP, increasing its GDP by 2%.
-            //Its 0.2 million population also gain the same $1000 per capita GDP, though all at once as opposed to in 100 $10 divisions.
+            float baseGDPChange = 33000000f; //One investment gives a base amount of 0.033 Bn GDP
 
-            float baseGDPChange = 70000000; //One investment gives 0.07 Bn GDP -- this gives approx 0.7% monthly economic growth if at 100% economic investment priority
-            float resourceRegionsBonus = __instance.currentResourceRegions * 7000000; //Plus 0.007 Bn per resource region
-            float coreEcoRegionsBonus = __instance.currentCoreEconomicRegions * 7000000; //Plus 0.007 Bn per core economic region
-            //TODO consider whether the way resource and economic regions scale economic growth relative to country size is appropriate
+            //Resource and core economic region multiplier
+            //Get a diminishing-return % bonus to growth based on total number of these regions
+            int totalRegions = __instance.currentResourceRegions + __instance.currentCoreEconomicRegions;
+            float regionsMult = 1f;
+            if (totalRegions >= 1) regionsMult += 0.2f; //20% bonus for first region
+            if (totalRegions >= 2) regionsMult += 0.1f; //10% bonus for second region
+            if (totalRegions >= 3) regionsMult += 0.05f; //5% bonus for third region
+            if (totalRegions >= 4) regionsMult += 0.02f * (totalRegions - 3); //2% bonus for fourth region and beyond
 
-            float summedGDPChange = baseGDPChange + resourceRegionsBonus + coreEcoRegionsBonus;
+            //Demographic stat bonuses
+            float educationMult = 1f + (0.15f * __instance.education); //get 15% bonus growth per education point
+            float democracyMult = 1f + (0.05f * __instance.democracy); //get 5% bonus growth per democracy point
+            float cohesionMult = 1.2f - (Mathf.Abs(__instance.cohesion - 5) * 0.04f); //at 5 cohesion, get 120% growth, reduced by 4% per point away from 5
+
+            //Per capita GDP multiplier
+            //This is an exponential decay function that gives countries with very low GDP per capita a large (up to 6 times!) bonus to growth
+            //This is 6 for a country with 0 gdp per capita, about 3.25 for a country with 15k, 1.76 for a country with 30k, 1 for a country with 44k, and 0.34 for a country with 70k
+            //In other words, countries with low gdp per capita will grow their absolute gdp per capita much faster than a country with higher, with things really speeding up for the first 15-20k, and really slowing down after 50k gdp per capita
+            float perCapGDPMult = 6f * Mathf.Pow(0.96f, __instance.perCapitaGDP / 1000f);
 
 
-            float democracyMult = 0.8f + (__instance.democracy * (0.4f / 10f)); //get 80% of the gdp at 0 democracy, 120% at 10 democracy
-            float cohesionMult = 1.1f - (Mathf.Abs(__instance.cohesion - 5f) * (0.2f / 5f)); //get 90% of gdp at 0 or 10 cohesion, 110% at 5 cohesion
-            float educationMult = 0.7f + (__instance.education * (0.9f / 10f)); //get 70% of the gdp at 0 education, 160% at 10 education, and even more at higher education values
+            float modifiedGDPChange = baseGDPChange * regionsMult * educationMult * democracyMult * cohesionMult * perCapGDPMult; //Final amount of gdp to gain
 
-            float perCapGDPMult = 5f - Mathf.Min(4f, __instance.perCapitaGDP * (4f / 15000f)); //Poor nation catch-up: Up to a 500% gdp gain modifier at close to 0 gdp per capita, returning to 100% at 15k gdp per capita and staying at 100% for higher values
-
-            float finalGDPChange = summedGDPChange * democracyMult * cohesionMult * educationMult * perCapGDPMult; //Final value for overall country GDP change taking all modifiers into account
-            
-
-            float finalPerCapGDPChange = finalGDPChange / __instance.population; //Amount to change GDP per capita by
+            float finalPerCapGDPChange = modifiedGDPChange / __instance.population; //Amount to change GDP per capita by
 
             __result = finalPerCapGDPChange;
 
