139f1419d788c68940201a3e4efba3f50d84fee8
gorion.b <gorion.b@gmail.com>
2022-10-29 18:34:04 -0500

    Adjusted spoils money to have a greater but diminishing returns impact from resource regions

--- TIEconomyMod/SpoilsInvestmentPatches/SpoilsMoneyEffectPatch.cs
+++ TIEconomyMod/SpoilsInvestmentPatches/SpoilsMoneyEffectPatch.cs
@@ -16,13 +16,20 @@ namespace TIEconomyMod.SpoilsInvestmentPatches
         {
             //Patch changes the instant money effect of a spoils investment to be a flat value, not scaled by nation size
 
-            //A Vanilla nation with 10 IP can extract about 2000 money/month, at the cost of 30 control cap for 66 monthly money per control cost
-            //To maintain balance, lets keep that monthly money to control cost rate the same, which means a spoils reward of about 66 money for 1 IP (which costs 1 control cost)
-            float baseMoneyGained = 30f; //Base 30 money
-            float resourceRegionBonusMoney = 3f * __instance.currentResourceRegions; //3 money per resource region
-            float lowDemocracyBonusMoney = 1.5f * (10f - __instance.democracy); //2 money per democracy less than 10
+            float baseMoneyGained = 20f; //Base 20 money
 
-            __result = baseMoneyGained + resourceRegionBonusMoney + lowDemocracyBonusMoney;
+            //Add money per resource region, with diminishing returns
+            float resourceRegionBonusMoney = 0f;
+            if (__instance.currentResourceRegions >= 1) resourceRegionBonusMoney += 10f; //10 extra for the 1st region
+            if (__instance.currentResourceRegions >= 2) resourceRegionBonusMoney += 5f; //5 extra for the 2nd
+            if (__instance.currentResourceRegions >= 3) resourceRegionBonusMoney += 2f; //1 extra for the 3rd
+            if (__instance.currentResourceRegions >= 4) resourceRegionBonusMoney += 1f * (__instance.currentResourceRegions - 3); //1 extra for the 4th and on
+
+
+            //Up to 50% extra money at 0 democracy, 0% extra at 10 democracy
+            float democracyMult = 1.5f - (__instance.democracy * (0.5f / 10f));
+
+            __result = (baseMoneyGained + resourceRegionBonusMoney) * democracyMult;
 
 
             return false; //Skip original getter
