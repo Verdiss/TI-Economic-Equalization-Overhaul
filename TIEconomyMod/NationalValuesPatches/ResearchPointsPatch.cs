0cbdd85dc5bf3f556c24ae14a2fb090872c61964
gorion.b <gorion.b@gmail.com>
2022-10-29 17:14:33 -0500

    Adjusted base research and research scaling from education to be less biased towards well educated nations

--- TIEconomyMod/NationalValuesPatches/ResearchPointsPatch.cs
+++ TIEconomyMod/NationalValuesPatches/ResearchPointsPatch.cs
@@ -17,15 +17,17 @@ namespace TIEconomyMod
         {
             //Patches the amount of research points available to a nation
             //This removes both the flat research amount every country gets based on its education score, and the ^1.1 scaling on population size
-            //The goal is for research points to roughly equal 1.5-2 times the control cost of the nation, for a sufficiently educated and wealthy nation
+            
             
             //Now, it would be nice to go into a really precise and comprehensive calculation here to figure out how much research per million pop I should give
             //But the reality is that this is way too complicated a relationship between population, GDP, GDP per capita, and the various multipliers here
             //So I'm gonna go with old faithful: Guess and check some values until it's where I want it to be.
 
-            float populationResearch = __instance.population_Millions * 0.00292f; //Removed better-than-linear scaling, increased vanilla 0.00225 multiplier to 0.00292 (30% increase)
+            float baseResearch = 0.00400f; //Increased vanilla 0.00225 multiplier to 0.00400
+            
+            float popMultiplier = __instance.population_Millions; //Removed vanilla better-than-linear scaling.
 
-            float educationEffect = Mathf.Pow(__instance.education, 2.1f); //Changed from vanilla ^2, now get 126x research at 10 education than at 1 (rather than 100x)
+            float educationEffect = Mathf.Pow(__instance.education, 2.0f); //Vanilla power 2
 
             float gdpPerCapEffect = Mathf.Max(__instance.perCapitaGDP / 20000f, 0.6f); //Floored at 60% research at 12000 gdp per capita, 200% at 40k, and indefinitely upward
             float democracyEffect = Mathf.Pow(Mathf.Max(__instance.democracy, 0.1f), 0.2f); //Vanilla, gives about 60% boost at 10 democracy, 40% penalty at 0
@@ -34,7 +36,7 @@ namespace TIEconomyMod
 
             float advisorBonus = (1f + __instance.adviserScienceBonus); //Vanilla, up to 25% boost at 25 councilor science score
 
-            __result = populationResearch * educationEffect * gdpPerCapEffect * democracyEffect * cohesionEffect * unrestEffect * advisorBonus;
+            __result = baseResearch * popMultiplier * educationEffect * gdpPerCapEffect * democracyEffect * cohesionEffect * unrestEffect * advisorBonus;
 
             return false; //Skip original getter
         }
